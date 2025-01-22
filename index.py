7361c1046e5d881e498abbbb8ddb0367b0f7c7f1
Carkey-Aakash <carkeyaakash360@gmail.com>
2025-01-22 21:58:15 +0545

    while loop changes

--- index.py
+++ index.py
@@ -1,8 +1,23 @@
-def scoreboard():
-    print(f"0|1|2")
-    print("...|-|--")
-    print(f"3|4|5")
-    print(f"6|7|8")
+def printboard(xstate,zstate):
+    print(f"0 | 1 | 2")
+    print(f"--|---|--")
+    print(f"3 | 4 | 5")
+    print(f"--|---|--")
+    print(f"6 | 7 | 8")
 
+
+xstate=[0,0,0,0,0,0,0,0,0]
+zstate=[0,0,0,0,0,0,0,0,0]
 print("Welcome to the tic tac toe game: ")
-scoreboard()
+turn=1
+while(True):
+    if(turn==1):
+        {
+          print("X-choice")
+        }
+    else:
+        print("O-choice")
+   
+        
+   
+   
